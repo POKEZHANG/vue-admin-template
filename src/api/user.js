1b9d36db65cc342fe3a47365e154d0454aee367d
POKEZHANG <791557421@qq.com>
2025-02-07 14:33:42 +0800

    docs 增加代码注释

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,28 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
+/**
+ * 用户相关接口
+ * 包含登录、获取用户信息、登出等功能
+ */
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
-    method: 'post',
-    data
-  })
+    url: "/vue-admin-template/user/login",
+    method: "post",
+    data,
+  });
 }
 
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
-    method: 'get',
-    params: { token }
-  })
+    url: "/vue-admin-template/user/info",
+    method: "get",
+    params: { token },
+  });
 }
 
 export function logout() {
   return request({
-    url: '/vue-admin-template/user/logout',
-    method: 'post'
-  })
+    url: "/vue-admin-template/user/logout",
+    method: "post",
+  });
 }
